fb3a12531023a056766c816becf03cab60d2dadf
antichamber <redstonersunite@gmail.com>
2017-01-18 09:06:49 -0700

    Add files via upload

---
+++ include/main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <API.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define piston_a 2
+#define piston_b 3
+#define left_quad_top 4
+#define left_quad_bottom 5
+#define right_quad_top 6
+#define right_quad_bottom 7
+#define leda = 8
+#define ledb = 9
+#define ledc = 10
+//#define button_enter 11
+#define button_select 12
+#define left_motor_a 10
+#define left_motor_b 8
+#define right_motor_a 1
+#define right_motor_b 3
+#define arm_motor_a 9
+#define arm_motor_b 2
+#define arm_motor_c 5
+#define arm_motor_d 6
+#define claw_motor_a 7
+#define claw_motor_b 4
+#define arm_pot_a 1
+#define arm_pot_b 2
+#define arm_adjust_a 100
+#define claw_adjust_a 100
+#define claw_adjust_b 50
+
+void autonomous();
+void initializeIO();
+void initialize();
+void operatorControl();
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/opcontrol.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include "API.h"
+#include "test.h"
+//==========================================================-GLOBAL INTERGERS AND FUNCTIONS GO HERE-============================================================
+bool special_test = false;
+bool special_toggle = true;
+bool assist = false;
+int left_mem = 0;
+int right_mem = 0;
+int claw_timeout = 0;
+bool claw_thing = 0;
+bool test = 0;
+
+void operatorControl() {
+	while (1) {
+    left_motor_speed = joystickGetAnalog(1, 2);
+    right_motor_speed = joystickGetAnalog(1, 3);
+		claw_motor_speed = 0;
+		claw_timeout++;
+		if(joystickGetDigital(1,8, JOY_UP) == 1){
+			claw_thing = 1;
+		}else if(joystickGetDigital(1,7, JOY_UP) == 1){
+			claw_thing = 0;
+		}
+		if(claw_thing == 1){
+			arm_motor_speed = -50;
+		}
+		if(joystickGetDigital(1,5, JOY_UP) == 1){
+			claw_motor_speed = -127;
+			claw_timeout = 0;
+		}else if(joystickGetDigital(1,5, JOY_DOWN) == 1){
+			if(claw_timeout < 100){
+				claw_motor_speed = 127;
+			}else{
+				claw_motor_speed = 60;
+			}
+		}else{
+				claw_timeout = 0;
+		}
+		arm_motor_speed = 0;
+		if(joystickGetDigital(1,6, JOY_UP) == 1){
+			arm_motor_speed = 127;
+			if(assist == true){
+				assist_piston = 1;
+			}else if(assist == false){
+				assist_piston = 0;
+			}
+			left_mem = analogRead(arm_pot_a);
+		}else if(joystickGetDigital(1,6, JOY_DOWN) == 1){
+			arm_motor_speed = -127;
+			assist_piston = 0;
+			left_mem = analogRead(arm_pot_a);
+		}
+		if(joystickGetDigital(1,5, JOY_UP) == 1){
+		}else if(joystickGetDigital(1,5, JOY_DOWN) == 1){
+		}
+		update_hardware();
+	}
+}
